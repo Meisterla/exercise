07398da2f52846ca1a33b09c2bde0b8ffb98957d
Meisterla <1185676721@qq.com>
2021-11-04 10:52:46 +0000

    update

--- ex03.py
+++ ex03.py
@@ -3,21 +3,44 @@ import numpy as np
 result = []
 
 
-def queen(n, cur=0):
-    if cur == len(n):
-        print(n)
-        return 0
-    for col in range(len(n)):
-        n[cur] = col
-        flag = True
-        for row in range(cur):
-            if n[row] == col or col - n[row] == cur - row or col - n[row] == row - cur:
-                flag = False
-                break
-        if flag:
-            queen(n, cur+1)
+def printgrip(chess, n):
+
+    lst = n * [n * ['1']]
+    grip = np.array(lst)
+    for i in range(n):
+        grip[i][chess[i]] = 'Q'
+    print(grip)
 
 
-queen([None]*4)
+def conflict(pos, chess):
+    len_chess = len(chess)
 
-#print(result)
+    for i in range(len_chess):
+        if abs(chess[i] - pos) in (0, len_chess - i):
+            return True
+    return False
+
+
+def solve(n, chess):
+    if len(chess) == n:
+        result.append(chess)
+        #printgrip(chess, n)
+
+    else:
+        for pos in range(n):
+            if not conflict(pos, chess):
+                chess1 = []
+                for i in chess:
+                    chess1.append(i)
+                chess1.append(pos)
+                solve(n, chess1)
+
+
+solve(8, [])
+for i in result:
+    printgrip(i, 8)
+    more = input('More?')
+    if more == 'more':
+        continue
+    else:
+        break
