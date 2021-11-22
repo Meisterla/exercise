8158bf0bbea97812f8ecf9701872598529258453
Meisterla <1185676721@qq.com>
2021-11-22 16:10:07 +0000

    update

---
+++ ex04.py
@@ -0,0 +1,12 @@
+"""
+@author        Shuo Qiu
+@date          2021/11/22
+@describe
+"""
+
+class Directory:
+    def __init__(self, pathname, files):
+        self.pathname = pathname
+        self.files = files
+
+
