87782a616c458f83abc36e3925d54dd9ccefbaff
Meisterla <1185676721@qq.com>
2021-11-23 22:26:56 +0000

    update

--- ex04.py
+++ ex04.py
@@ -4,9 +4,43 @@
 @describe
 """
 
-class Directory:
-    def __init__(self, pathname, files):
-        self.pathname = pathname
+
+def print_tree(directory):
+    result = ""
+    result += directory.directory_name
+    return result
+
+
+class Tree:
+    pass
+
+
+class Directory(Tree):
+    def __init__(self, directory_name, files):
+        self.directory_name = directory_name
         self.files = files
 
+    def __str__(self):
+        return print_tree(self)
 
+
+class PlainFile(Tree):
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+    def __str__(self):
+        return f"{self.file_name}"
+
+
+# file = PlainFile("boot.exe")
+# folder = Directory("Downloads", [])
+root = Directory("root",
+                 [PlainFile("boot.exe"),
+                  Directory("home",
+                            [Directory("thor",
+                                       [PlainFile("hunde.jpg"),
+                                        PlainFile("quatsch.txt")]),
+                             Directory("isaac", [PlainFile("gatos.jpg")])])])
+
+
+print(root)
