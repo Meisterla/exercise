cbeaa7fcdf79351663ca6f5959ed802150d6b888
Meisterla <1185676721@qq.com>
2021-11-23 23:02:18 +0000

    update

--- ex04.py
+++ ex04.py
@@ -5,12 +5,6 @@
 """
 
 
-def print_tree(directory):
-    result = ""
-    result += directory.directory_name
-    return result
-
-
 class Tree:
     pass
 
@@ -19,20 +13,28 @@ class Directory(Tree):
     def __init__(self, directory_name, files):
         self.directory_name = directory_name
         self.files = files
+        self.owner = "default"
 
     def __str__(self):
-        return print_tree(self)
+        return f'"{self.directory_name}"'
+
+    def chown(self, new_owner):
+        self.owner = new_owner
 
 
 class PlainFile(Tree):
     def __init__(self, file_name):
         self.file_name = file_name
+        self.owner = "default"
 
     def __str__(self):
-        return f"{self.file_name}"
+        return f'PlainFile("{self.file_name}")'
+
+    def chown(self, new_owner):
+        self.owner = new_owner
 
 
-# file = PlainFile("boot.exe")
+file = PlainFile("boot.exe")
 # folder = Directory("Downloads", [])
 root = Directory("root",
                  [PlainFile("boot.exe"),
@@ -43,4 +45,4 @@ root = Directory("root",
                              Directory("isaac", [PlainFile("gatos.jpg")])])])
 
 
-print(root)
+print(file)
